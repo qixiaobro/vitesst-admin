bcf0e0973a645a6d8e343465f826ae841c59569e
qixiaobro <982092690@qq.com>
2022-05-30 14:43:49 +0800

    feat: 图片资源

--- src/layout/Menu/components/Logo.vue
+++ src/layout/Menu/components/Logo.vue
@@ -1,13 +1,11 @@
-<template>
-  <div h-60px flex items-center justify-center :style="{ width: isCollapse ? '65px' : '220px' }">
-    <img :src="logo1" w-100px alt="logo" v-show="!isCollapse" />
-    <img :src="logo2" w-30px alt="logo" v-show="isCollapse" />
-  </div>
-</template>
-
 <script setup lang="ts">
-defineProps<{ isCollapse: boolean }>();
-const logo1 = new URL('src/assets/images/logo.png', window.location.href).href
-const logo2 = new URL('src/assets/images/logo-m.png', window.location.href).href
+defineProps<{ isCollapse: boolean }>()
 
 </script>
+
+<template>
+  <div h-60px flex items-center justify-center :style="{ width: isCollapse ? '65px' : '220px' }">
+    <img v-show="!isCollapse" src="../../../assets/images/logo.png" w-100px alt="logo">
+    <img v-show="isCollapse" src="../../../assets/images/logo-m.png" w-30px alt="logo">
+  </div>
+</template>
