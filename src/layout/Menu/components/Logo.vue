2a03e7e8a98ef8a675107f82c752b6dce2569719
qixiaobro <zxd982092690@gmail.com>
2022-06-30 23:36:27 +0800

    chore: 优化

--- src/layout/Menu/components/Logo.vue
+++ src/layout/Menu/components/Logo.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
 defineProps<{ isCollapse: boolean }>()
-
 </script>
 
 <template>
   <div h-60px flex items-center justify-center :style="{ width: isCollapse ? '65px' : '220px' }">
-    <img v-show="!isCollapse" src="../../../assets/images/logo.png" w-100px alt="logo">
+    <img v-show="!isCollapse" src="../../../assets/images/logo.png" w-50px alt="logo">
     <img v-show="isCollapse" src="../../../assets/images/logo-m.png" w-30px alt="logo">
   </div>
 </template>
